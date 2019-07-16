65f389cf216e1f6279596033ba714f7a87403e58
moon2850088 <zhou33750@gmail.com>
2019-07-15 22:00:00 -0400

    route part done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,7 +8,6 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
 
   heroes: Hero[];
 
@@ -20,10 +19,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect (hero: Hero): void {
-    this.selectedHero = hero;
   }
 
 }
