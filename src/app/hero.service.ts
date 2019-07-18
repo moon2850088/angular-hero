fcdf40bf1803b6642a3f677654757551b3315d36
moon2850088 <zhou33750@gmail.com>
2019-07-17 23:41:04 -0400

    delete hero done

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -69,4 +69,24 @@ export class HeroService {
 
   }
 
+  deleteHero (hero: Hero | number):Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`delete hero id=${id}`)),
+      catchError(this.handleError<Hero>(`deleteHero`))
+    );
+  }
+
+  searchHeroes(term: string):Observable<Hero[]> {
+    if(!term.trim()){
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes',[]))
+    );
+  }
+
 }
